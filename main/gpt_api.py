f8316a83596e1b9eecb3c4ea6d0711241e166a84
Jeremy Bui <91503410+bui-jeremy@users.noreply.github.com>
2023-05-03 00:19:49 -0400

    Update gpt_api.py

--- main/gpt_api.py
+++ main/gpt_api.py
@@ -1,7 +1,7 @@
 import openai
 
 def get_gpt_api(article_content):
-    openai.api_key = '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'
+    openai.api_key = ''
     prompt = f"How can I mediate this vulernability (If no vulerability is found, print 'False', nothing else): {article_content}"
     response = openai.Completion.create (
         engine= "text-davinci-003",
